90189d351f334f049954d5dc0f33ba3be9783de8
Son Pham <sonpx37@gmail.com>
2016-06-07 12:59:32 +0700

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Homy::Application.config.secret_key_base = 'daeb9211f2b1f3ae9388770bb8d09c58fb1dc6c33f4f17a26b86924f13bd550ae6ee807f91a9ccba0632248d0abd7116305047d90924714fbb63249255610a71'
