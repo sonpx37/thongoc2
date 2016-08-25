3a35b3ed3996c48d5fa1bafeb41b12b5d8949145
SonPX <sonpx37@gmail.com>
2016-08-25 22:56:43 +0700

    Update map for g

--- app/controllers/admissions_controller.rb
+++ app/controllers/admissions_controller.rb
@@ -1,6 +1,8 @@
 class AdmissionsController < ApplicationController
 
   def tuyen_sinh
-    
+     set_meta_tags title: t(".title", year:  Time.zone.now.year),
+              description: t(".description"),
+              keywords: t(".keywords")
   end
 end
